8f643b76cb73c072153b0b58884e6bcd226067ad
Chan-25 <57619252+Chan-25@users.noreply.github.com>
2023-01-31 14:08:27 +0900

    2023-01-31 조영찬 SynopsysPage 추가

--- front-end/fortoon/src/component/route/RouterComponent.jsx
+++ front-end/fortoon/src/component/route/RouterComponent.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import TagPage from '../webtoon/TagPage';
 import TestInfo from '../webtoon/TestInfo';
+import SysnopsysPage from '../webtoon/SynopsysSelectPage'
 
 const AppRouter = () => {
     return (
@@ -10,6 +11,7 @@ const AppRouter = () => {
                 <Routes>
                     <Route path="/" element={<TestInfo />} />
                     <Route path="/TagPage" element={<TagPage />} />
+                    <Route path="/Synopsys" element={<SysnopsysPage />} />
                 </Routes>
             </BrowserRouter>
         </div>
